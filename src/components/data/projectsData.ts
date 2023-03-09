7ba9b7c66f3760278b35822b918af8f4521a4770
ido evergeen <dicksonevegreenido@gmail.com>
2023-03-09 02:19:16 +0100

    fix issue on mobile

--- src/components/data/projectsData.ts
+++ src/components/data/projectsData.ts
@@ -11,4 +11,18 @@ export const projectsData = [
     color: "from-pink-500 to-yellow-500 bg-gradient-to-l",
     link: "/",
   },
+
+  {
+    name: "Project 3",
+    desc: "in testing ",
+    color: "from-red-500 to-purple-500 bg-gradient-to-r",
+    link: "/",
+  },
+
+  {
+    name: "Project 4",
+    desc: "in testing ",
+    color: "from-amber-500 to-gray-500 bg-gradient-to-r",
+    link: "/",
+  },
 ];
