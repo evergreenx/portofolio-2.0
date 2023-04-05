0779752616fa20888e4ce7c44b8b681fa8584004
ido evergeen <dicksonevegreenido@gmail.com>
2023-04-05 22:22:17 +0100

    add  new project

--- src/components/data/projectsData.ts
+++ src/components/data/projectsData.ts
@@ -1,6 +1,6 @@
 export const projectsData = [
   {
-    id:1,
+    id: 1,
     name: " Wordium doc",
     desc: "A word processor web application that allows you to create, edit, and share documents.",
     image:
@@ -9,51 +9,53 @@ export const projectsData = [
     ghlink: "https://github.com/evergreenx/Wordium-Docs",
     livelink: "https://wordiumdocsx.netlify.app/",
   },
-{
-id: 5,
-name: “bellafood”,
-desc:” A food delivery app with 7 screens”,
-image: “https://res.cloudinary.com/evergreenx/image/upload/v1679484580/Cargo_watch_presentation_fzo1pn.png”,
-ghlink:”https://github.com/evergreenx/bellafoodd”,
-color: "bg-amber-400",
-
-},
   {
-    id:2,
+    id: 5,
+    name: "Bellafood",
+    desc: " Why stay hungry when you can order form Bella Onojie's food store and get it delivered with ease",
+    image:
+      "https://res.cloudinary.com/evergreenx/image/upload/v1679484580/Cargo_watch_presentation_fzo1pn.png",
+    ghlink: "https://github.com/evergreenx/bellafoodd",
+    livelink: "https://github.com/evergreenx/bellafoodd",
+    color: "bg-blue-600",
+  },
+
+  {
+    id: 5,
+    name: "Dinter",
+    desc: " Dinter is a dating app that allows users to find their perfect match and chat with them. It is built with React Native and Firebase. ",
+    image:
+      "https://res.cloudinary.com/evergreenx/image/upload/v1680726977/Group_17_sad4d2.png",
+    ghlink: "https://github.com/evergreenx/dinter-",
+    livelink: "https://github.com/evergreenx/dinter-",
+    color: "bg-blue-600",
+  },
+
+
+
+
+
+  {
+    id: 2,
     name: "Look",
     desc: "A text translation application that helps users translate text from one language to another using Microsoft azure cognitive services translator services. ",
     color: "bg-amber-400",
     link: "/",
-    image:'https://res.cloudinary.com/evergreenx/image/upload/v1647727542/amazon2-preview_rmrj0k.png',
+    image:
+      "https://res.cloudinary.com/evergreenx/image/upload/v1647727542/amazon2-preview_rmrj0k.png",
     ghlink: "https://github.com/evergreenx/Look",
     livelink: "https://look-tau.vercel.app/",
   },
-  
-   {
-    id:3,
+
+  {
+    id: 3,
     name: "One Weather",
     desc: "A weather app that allows users to check the current weather and five-day forecast for any location.. ",
-    color: "bg-amber-200",
+    color: "bg-amber-700",
     link: "/",
-    image:'https://res.cloudinary.com/evergreenx/image/upload/v1680602048/p3_fxduhu.png',
+    image:
+      "https://res.cloudinary.com/evergreenx/image/upload/v1680602048/p3_fxduhu.png",
     ghlink: "https://github.com/evergreenx/oneweather",
     livelink: "https://oneweather-ashy.vercel.app/",
-  }
-
-  // {
-  //   id:"3",
-  //   name: "Project 3",
-  //   desc: "in testing ",
-  //   color: "from-red-500 to-purple-500 bg-gradient-to-r",
-  //   link: "/",
-  // },
-
-  // {id:4,
-  //   name: "Project 4",
-  //   desc: "in testing ",
-  //   color: "from-amber-500 to-gray-500 bg-gradient-to-r",
-  //   link: "/",
-  // },
-
-  
+  },
 ];
